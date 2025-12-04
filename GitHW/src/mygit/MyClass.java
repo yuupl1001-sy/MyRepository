e93de3cb25a5cd985c2dc8531f3bed0c522e21fc
조재호 <@CJH>
2025-12-04 14:26:02 +0900

    Add a println line

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -2,5 +2,7 @@ package mygit;
 public class MyClass {
 public static void main(String[] args) {
 System.out.println("Hello Git!");
+System.out.println("This is the 2nd version."); // 이 줄을 추가하세요
 }
 }
+
