8aa320ca4de66042c9b14c587771c325ea1ba370
조재호 <@CJH>
2025-12-11 19:07:09 +0900

    Master modified C class

--- GitHW_Project/src/mygit/C.java
+++ GitHW_Project/src/mygit/C.java
@@ -3,5 +3,7 @@ package mygit;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("C.java: Initial version");
+		// Master 브랜치에서 내용을 다르게 추가함 (충돌 유도용)
+		System.out.println("Master modified C class");
 	}
 }
