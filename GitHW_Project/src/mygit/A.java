af88f8cfc76ed594aa7a50d2b3b85edcaa02739f
조재호 <@CJH>
2025-12-11 18:48:07 +0900

    Initial version of A, B, C

---
+++ GitHW_Project/src/mygit/A.java
@@ -0,0 +1,7 @@
+package mygit;
+
+public class A {
+	public static void main(String[] args) {
+		System.out.println("A.java: Initial version");
+	}
+}
---
+++ GitHW_Project/src/mygit/B.java
@@ -0,0 +1,7 @@
+package mygit;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("B.java: Initial version");
+	}
+}
