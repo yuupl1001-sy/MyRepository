c9250ffc586f65ee2571bba37873c481c65eada1
조재호 <@CJH>
2025-12-11 19:03:37 +0900

    Alice modified A and C

--- GitHW_Project/src/mygit/C.java
+++ GitHW_Project/src/mygit/C.java
@@ -3,5 +3,7 @@ package mygit;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("C.java: Initial version");
+		System.out.println("Alice modified not B, C class");
+		
 	}
 }
