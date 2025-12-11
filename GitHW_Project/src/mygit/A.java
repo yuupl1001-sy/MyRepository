622c3e426393859024fb957f1981e1a9a998f3de
조재호 <@CJH>
2025-12-11 19:10:17 +0900

    Bob modified A and B

--- GitHW_Project/src/mygit/A.java
+++ GitHW_Project/src/mygit/A.java
@@ -3,5 +3,7 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("A.java: Initial version");
+		// Bob이 내용을 추가함 (나중에 Alice 내용과 충돌날 예정)
+		System.out.println("Bob modified A class");
 	}
 }
